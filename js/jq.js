a2558648b5db1823b3afe6c1170923233a489dea
Marc Cook <marcc24k@gmail.com>
2017-04-21 01:04:49 -0400

    lots of changes

---
+++ js/jq.js
@@ -0,0 +1,59 @@
+
+//BUTTONS FADE WHEN SCROLL UP
+
+$(window).scroll(function(){
+//    $(".button-one, .button-two").css("opacity", 1 - $(window).scrollTop() / 650);
+//     $("section").css("opacity", 1 - $(window).scrollTop() / 650);
+    
+//    var scrollTop = 70;
+//    
+//    
+//     if ($(this).scrollTop()>scrollTop){
+//           // animate fixed div to small size:
+//    $(".thumbnail-one").css("opacity", 1 - $(window).scrollTop() / 650);
+//       
+//     }  
+//    
+//           //  animate fixed div to original size
+//    if($(this).scrollTop()>scrollTop) {
+//           $(".thumbnail-two").css("opacity", 1 - $(window).scrollTop() / 1290);
+//        
+//           
+//       }
+//    
+//    if ($(this).scrollTop()>scrollTop){
+//           $(".thumbnail-three").css("opacity", 1 - $(window).scrollTop() / 2200);   
+//       }
+//        if ($(this).scrollTop()>scrollTop){
+//           $("#article-section").css("opacity", 1 - $(window).scrollTop() / 3200);
+//       }
+    
+
+    
+//      if ($(this).scrollTop()>675){
+//            // animate fixed div to small size:
+//            $("#city").stop().animate({ height: 90 , 'padding-top': 20},100);
+//        } else {
+//            //  animate fixed div to original size
+//            $("#city").stop().animate({ height: 675, 'padding-top': 0},100);
+//        }
+  });
+
+
+
+ 
+
+
+
+
+
+//var myVar;
+//
+//function myFunction() {
+//    myVar = setTimeout(showPage, 3000);
+//}
+//
+//function showPage() {
+//  document.getElementById("loader").style.display = "none";
+//  document.getElementById("myDiv").style.display = "block";
+//}
